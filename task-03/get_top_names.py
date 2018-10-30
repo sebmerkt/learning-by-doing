fedf09b8d15a27b60248c1fd65d82d03ae3c599a
semerkt <sebastian.andreas.merkt@cern.ch>
2018-10-30 13:25:29 -0400

    Update

--- task-03/get_top_names.py
+++ task-03/get_top_names.py
@@ -5,21 +5,27 @@ https://github.com/astropgh/learning-by-doing/tree/master/task-03
 """
 
 def extract_data_lines(filename, start_text, end_text):
-    """
-    open `filename`, and yield the lines between
-    the line that contains `start_text` and the line that contains `end_text`
-    """
-    # fill in code as needed
-    with open(filename) as fh:
-        for line in fh:
-            # fill in code as needed
-            # use `yield line` to return desired lines but keep the function going
+  """
+  open `filename`, and yield the lines between
+  the line that contains `start_text` and the line that contains `end_text`
+  """
+  # fill in code as needed
+  with open(filename) as fh:
+    start_it=False
+    for line in fh:
+        # fill in code as needed
+        # use `yield line` to return desired lines but keep the function going
+        if start_text in line:
+          start_it=True
+        if start_it:
+          yield line
+          if end_text in line:
+            break
 
+# if __name__ == '__main__':
+filename = 'top5names.html'
+start_text = '<tr><td align="center">2017</td>'
+end_text = '</table></center></div><!-- end #content -->'
 
-if __name__ == '__main__':
-    filename = 'top5names.html'
-    start_text = '<tr><td align="center">2017</td>'
-    end_text = '</table></center></div><!-- end #content -->'
-
-    for line in extract_data_lines(filename, start_text, end_text):
-        print(line)
+for line in extract_data_lines(filename, start_text, end_text):
+    print(line)
